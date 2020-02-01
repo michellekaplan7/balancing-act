eddffdeb5631d9b493180696423a075ae8e9d168
Michelle Kaplan <kaplan.michelle7@gmail.com>
2020-02-01 08:29:23 -0700

    Add JS EventListener for blue-border on click to dashboard and wallet icon

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ closeButton.addEventListener('click', closeWelcome);
 function closeWelcome() {
   welcomeBannerDisappear.style.display = "none";
 }
+
+var dashboardIcon = document.querySelector('.dashboard-icon');
+var walletIcon = document.querySelector('.transactions-icon');
+
+walletIcon.addEventListener('click', addWalletBoarder);
+
+function addWalletBoarder() {
+  walletIcon.classList.add('blue-border');
+  dashboardIcon.classList.remove('blue-border');
+}
+
+dashboardIcon.addEventListener('click', addDashboardBoarder);
+
+function addDashboardBoarder() {
+  dashboardIcon.classList.add('blue-border');
+  walletIcon.classList.remove('blue-border');
+}
+
+//body disappear and keep nav bar
+// var walletIcon = document.querySelector('.transactions-icon');
+//
+// walletIcon.addEventListener('click', hideBodyContent);
+//
+// function hideBodyContent() {
+//   walletIcon.classList.remove('')
+// }
